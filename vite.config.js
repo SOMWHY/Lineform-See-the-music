2f6c3864ea2ff6cb8835d9a765d44acd41cf8021
SOMWHY <1928135011@qq.com>
2025-09-12 23:26:16 +0800

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
     },
   },
   plugins: [react(), tailwindcss()],
-  base:'/Lineform-See-the-music/'
-});
+  base: "/",
+})
