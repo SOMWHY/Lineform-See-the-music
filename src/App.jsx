ed69b324274e80cc266365bc4f80c315d0d06ca2
SOMWHY <1928135011@qq.com>
2025-09-14 01:37:20 +0800

    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Playlist } from "./features/player/components/Playlist"
 import VisualOutput from "./features/visualizer/components/VisualOutput"
-import GlobalControlContainer from "./features/globalControl/components/globalControlContainer"
+import GlobalControlContainer from "./features/globalControl/components/GlobalControlContainer"
 import InfoDisplayContainer from "./features/infoDisplay/components/InfoDisplayContainer"
 const App = () => {
   return (
