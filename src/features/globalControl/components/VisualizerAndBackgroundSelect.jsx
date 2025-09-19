1e1bd56a49aaa7f1c61e2edaf822bd80602ecdda
SOMWHY <1928135011@qq.com>
2025-09-19 11:35:20 +0800

    Change select style

--- src/features/globalControl/components/VisualizerAndBackgroundSelect.jsx
+++ src/features/globalControl/components/VisualizerAndBackgroundSelect.jsx
@@ -7,36 +7,30 @@ const VisualizerAndBackgroundSelect = () => {
   const setBackground = useCanvasStore(state => state.setBackground)
   const setVisualizer = useCanvasStore(state => state.setVisualizer)
   return (
-    <div className='mt-xs px-md h-sm py-xs flex-center flex '>
-      <div className='gap-md flex w-[100%] items-center justify-start font-comingSoon text-lg'>
-        <select
-          className='select'
-          name='background'
-          id='background'
-          value={background}
-          onChange={e => setBackground(e.target.value)}
-        >
-          <option className='option' value='none'>
-            none
-          </option>
-          <option className='option' value={BACKGROUND.SKY}>
-            sky
-          </option>
-        </select>
-        <select
-          className='select'
-          name='visualizer'
-          id='visualizer'
-          value={visualizer}
-          onChange={e => setVisualizer(e.target.value)}
-        >
-          <option className='option' value='none'>
-            none
-          </option>
-          <option className='option' value={VISUALIZER.LINE_FORM}>
-            lineform
-          </option>
-        </select>
+    <div className='mt-xs px-md  py-xs flex-center flex '>
+      <div className='gap-2xs flex flex-col w-[100%]  justify-center items-center font-comingSoon tracking-tight text-lg'>
+        <div className='flex  w-full justify-between pr-lg'>
+          <label htmlFor='background'>Background:</label>
+          <select className='select' name='background' id='background' value={background} onChange={e => setBackground(e.target.value)}>
+            <option className='option' value='none'>
+              none
+            </option>
+            <option className='option' value={BACKGROUND.SKY}>
+              sky
+            </option>
+          </select>
+        </div>
+        <div className='flex w-full justify-between pr-lg'>
+          <label htmlFor='visualizer'>Visualizer:</label>
+          <select className='select' name='visualizer' id='visualizer' value={visualizer} onChange={e => setVisualizer(e.target.value)}>
+            <option className='option' value='none'>
+              none
+            </option>
+            <option className='option' value={VISUALIZER.LINE_FORM}>
+              lineform
+            </option>
+          </select>
+        </div>
       </div>
     </div>
   )
